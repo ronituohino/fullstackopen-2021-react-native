031af0ffabcef327d5f9767d966d9bfce559238d
Roni Tuohino <42767842+ronituohino@users.noreply.github.com>
2021-09-25 14:03:29 +0300

    10.12 done!

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,11 +1,13 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import Constants from 'expo-constants';
 
 const httpLink = createHttpLink({
   // Replace the IP address part with your own IP address!
-  uri: 'http://87.92.156.33:5000/graphql',
+  uri: Constants.manifest.extra.apolloUri,
 });
 
 const createApolloClient = () => {
+  console.log(Constants.manifest);
   return new ApolloClient({
     link: httpLink,
     cache: new InMemoryCache(),
