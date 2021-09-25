edb06d88805bdf621d104de813da91cd38012a9a
Roni Tuohino <42767842+ronituohino@users.noreply.github.com>
2021-09-25 13:34:13 +0300

    10.11 done!

---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,12 @@
+import { useQuery } from '@apollo/client';
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+const useRepositories = () => {
+  const { loading, data, refetch } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network'
+  });
+
+  return { repositories: data ? data.repositories : undefined, loading, refetch };
+};
+
+export default useRepositories;
